ad4fff7551943de06793b280771116c8fe5a8df5
Petr Damborsky <petr.damborsky@gmail.com>
2015-07-14 23:23:59 +0200

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+	res.send('hello');
+});
+
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('Listening at http://%s:%s', host, port);
+});
+
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request: ' + req.body);
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
