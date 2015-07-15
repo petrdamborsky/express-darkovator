78bd24c90ae9fc5dab5a2426a02530625952a41c
Petr Damborsky <petr.damborsky@gmail.com>
2015-07-15 23:28:48 +0200

    Adding Jade

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', './views');
+
 app.get('/', function(req,res){
-	res.send('hello');
+	res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 var server = app.listen(3000, function(){
 	var host = server.address().address;
 	var port = server.address().port;
-	
 	console.log('Listening at http://%s:%s', host, port);
-});
-
-// respond with "Hello World!" on the homepage
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-// accept POST request on the homepage
-app.post('/', function (req, res) {
-  res.send('Got a POST request: ' + req.body);
-});
-
-// accept PUT request at /user
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user');
-});
-
-// accept DELETE request at /user
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user');
 });
