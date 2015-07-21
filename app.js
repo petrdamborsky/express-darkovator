420739a33bd397be0f73f5e4404633e61ff7dee9
Petr Damborsky <petr.damborsky@gmail.com>
2015-07-21 07:54:05 +0200

    Basic routes config, wishlist template added

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var app = express();
+var routes = require('./routes/main.js');
 
+// configure view engine
 app.set('view engine', 'jade');
 app.set('views', './views');
 
-app.get('/', function(req,res){
-	res.render('index', { title: 'Hey', message: 'Hello there!'});
-});
+// configure public folder
+app.use(express.static('public'));
+
+// configure routes
+app.use('/',routes);
 
 var server = app.listen(3000, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Listening at http://%s:%s', host, port);
 });
+
+app.use('/book/:id', function (req, res, next) {
+    console.log('ID:', req.params.id);
+    next();
+});
+
+//error handling
+function logErrors(err, req, res, next) {
+    console.error(err.stack);
+    next(err);
+}
---
+++ routes/main.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.use(function timeLog(req, res, next) {
+    console.log('Request at: ' + Date.now());
+    next();
+});
+
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
+})
+
+router.get('/wishlist', function (req, res) {
+    res.render('wishlist');
+});
+
+module.exports = router;
